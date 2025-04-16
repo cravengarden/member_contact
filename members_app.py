58646a7679efe5c0d65f2602804f39bad4afd7fa
cravengarden <craven.garden@btinternet.com>
2025-04-16 18:13:01 +0100

    Update members_app.py

--- members_app.py
+++ members_app.py
@@ -5,9 +5,11 @@ import pandas as pd
 sheet_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQGCIkQEvfia5GeNnTXAOi1CAAA-IcOZCgcR1hXvj-JIepsUn3tPqtIaB-56fFXl8yadsRMockQ7dPx/pub?output=csv"
 
 # Load member data
-@st.cache_data
+#@st.cache_data #commented out to prevent caching
 def load_data(url):
     return pd.read_csv(url)
+if st.button("🔄 Refresh Data"):
+    st.cache_data.clear()
 
 df = load_data(sheet_url)
 
